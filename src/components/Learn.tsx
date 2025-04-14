d17cb946393463c65d32a976a029994cdbfbb658
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-04-14 12:12:00 +0530

    Adjust RedFlagsList spacing

--- src/components/Learn.tsx
+++ src/components/Learn.tsx
@@ -107,3 +107,4 @@ export default Learn;
 // Refactor GuardianLevels component - 2025-04-11
 // Add link to RugPullBasics page - 2025-04-12
 // Enhance leaderboard data fetch - 2025-04-13
+// Adjust RedFlagsList spacing - 2025-04-14
