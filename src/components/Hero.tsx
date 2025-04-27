7c013bf9a45840cdb1778691493c219c71897817
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-04-27 12:14:00 +0530

    Fix header alignment issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -64,3 +64,4 @@ export function Hero() {
 // Fix header alignment issue - 2025-04-23
 // Optimize learningModules.js imports - 2025-04-25
 // Fix types in Learn.tsx - 2025-04-25
+// Fix header alignment issue - 2025-04-27
